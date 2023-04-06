9eda5008e90e043286ef459259c75378dfb956b0
Gnosis <emmagab38@gmail.com>
2023-04-06 09:39:15 +0100

    commit changes

--- resources/views/dashboard/payment.blade.php
+++ resources/views/dashboard/payment.blade.php
@@ -6,7 +6,7 @@
         <div class="w-full md:w-4/12 md:border-r md:border-gray-700 p-6">
             <h4 style="color: white; text-align: center; ">Payment QRCode ({{ optional($deposit->payment_method)->name }})</h4>
 
-{{--            {!! QrCode::size(200)->generate($deposit->payment_method->value ? : 'No wallet'); !!}--}}
+            {!! QrCode::size(200)->generate($deposit->payment_method->value ? : 'No wallet'); !!}
 
         </div>
 
